372927e5be9b957f8be7a2a057b88039adc50260
1982002782 <1982002782@qq.com>
2022-08-03 16:25:31 +0800

    学生管理系统

--- src/main/java/controller/TalkController.java
+++ src/main/java/controller/TalkController.java
@@ -1,8 +1,13 @@
 package controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import pojo.Talk;
 import service.TalkService;
+
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -11,6 +16,11 @@ public class TalkController {
 	@Autowired
 	private TalkService talkService;
 	
-	
+	@RequestMapping("queryAllTalks.do") 
+	public String queryAllTalks(Model model) {
+		List<Talk> talks = talkService.queryAllTalks();
+		model.addAttribute("talks", talks);
+		return "talk";
+	}
 	
 }
